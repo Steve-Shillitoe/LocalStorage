78834a709e58bd6fcda838f295a19bfb7cfb0d8c
IMI-TRISTAN <s.shillitoe@sheffield.ac.uk>
2023-12-30 02:15:14 +0000

    Add project files.

---
+++ wwwroot/scripts/bweInterop.js
@@ -0,0 +1,9 @@
+﻿//create a variable named bweInterop and assigns it an empty object { }.
+var bweInterop = {};
+
+bweInterop.setLocalStorage = function (key, data) {
+    localStorage.setItem(key, data);
+}
+bweInterop.getLocalStorage = function (key) {
+    return localStorage.getItem(key);
+}
